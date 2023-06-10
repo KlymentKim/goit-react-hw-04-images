220e439398630e7c5803ad95b141188f7447462d
Klyment <klim86kr@gmail.com>
2023-06-11 01:16:45 +0300

    add

--- src/components/ImageGalleryItem/ImageGalleryItem.styled.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.styled.js
@@ -5,6 +5,7 @@ export const GalleryImage = styled.img`
     height: 260px;
     object-fit: cover;
     transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    border-radius: 5px;
 
     :hover, :focus {
         transform: scale(1.03);
