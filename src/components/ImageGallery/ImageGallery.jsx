d903cb9ceb3501c87d5c7ea96146f47ec4c4efa5
Klyment <klim86kr@gmail.com>
2023-06-15 19:54:57 +0300

    fix

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { fetchPixabayApi } from '../Api/api';
+import { fetchPixabayApi } from '../Api/Api';
 import { GalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { ErrorMessage, GalleryList, ListItem } from './ImageGallery.styled';
 import { LoadMore } from 'components/Button/Button';
