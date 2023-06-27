32fb1dd5dac7025d9d3f8bb02b94e2b1d6556d49
Klyment <klim86kr@gmail.com>
2023-06-28 00:59:43 +0300

    fix

--- src/components/Searchbar/Searchbar.styled.js
+++ src/components/Searchbar/Searchbar.styled.js
@@ -18,6 +18,10 @@ export const SearchHead = styled.header`
   background-color: #20d462;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+
+    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
 `;
 
 export const SearchForm = styled.form`
