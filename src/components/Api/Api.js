decb1065ccc1678d05665fedb200c65c8293e141
Klyment <klim86kr@gmail.com>
2023-07-13 00:52:38 +0300

    fix

--- src/components/Api/Api.js
+++ src/components/Api/Api.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '37187693-cb92e988afefb94434f725c21';
-const IMAGE_TYPE = 'photo';
-const ORIENTATION = 'horizontal';
+export const IMAGE_TYPE = 'photo';
+export const ORIENTATION = 'horizontal';
 const SAFESEARCH = 'true';
-const quantityPage = 12;
+export const quantityPage = 12;
 
-export const fetchPixabayApi = async (inputValue, page) => {
+export const fetchPixabayApi = async (inputValue, page = 1) => {
     const response = await axios.get(
         `?key=${API_KEY}&q=${inputValue}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFESEARCH}&page=
         ${page}&${quantityPage}`
@@ -15,9 +15,9 @@ export const fetchPixabayApi = async (inputValue, page) => {
     return response.data;
 };
 
-// // Функция для нормализации массива изображений
-// export const normalizedImages = imagesArray =>
-//   imagesArray.map(({ id, tags, webformatURL, largeImageURL }) => {
-//     return { id, tags, webformatURL, largeImageURL };
-//   });
+// Функция для нормализации массива изображений
+export const normalizedImages = imagesArray =>
+  imagesArray.map(({ id, tags, webformatURL, largeImageURL }) => {
+    return { id, tags, webformatURL, largeImageURL };
+  });
 
